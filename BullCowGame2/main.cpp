0104f2a24058ef90ace48441e9a4d7293defd05a
LRBeaver <GitPass16!>
2017-05-17 07:31:44 -0400

    New

---
+++ BullCowGame2/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+void PrintIntro();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; // Instantiate a new game
+
+int main()
+{
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
+	return 0;
+}
+
+void PrintIntro()
+{
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
+
+void PlayGame()
+{
+	int MaxTries = BCGame.GetMaxTries();
+	std::cout << MaxTries << std::endl;
+	// loop for the number of turns asking for guesses
+	//constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= MaxTries; count++)
+	{
+		std::string Guess = GetGuess();
+		std::cout << "You guess was: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+std::string GetGuess()
+{
+	// get a guess from the player
+	int CurrentTry = ;
+	std::cout << "Enter your guess: ";
+	std::string Guess = "";
+
+	std::getline(std::cin, Guess);
+	return Guess;
+
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again (y/n)? ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	//std::cout << "Is it y? " << (Response[0] == 'y' || 'Y');
+
+
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+	std::cout << std::endl;
+}
